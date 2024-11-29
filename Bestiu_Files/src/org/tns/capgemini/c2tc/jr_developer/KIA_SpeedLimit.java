7392060d0f32a5b929250063a28fd05b50b83a15
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-11-30 00:02:40 +0530

    THIRD COMMIT

---
+++ Bestiu_Files/src/org/tns/capgemini/c2tc/jr_developer/KIA_SpeedLimit.java
@@ -0,0 +1,15 @@
+package org.tns.capgemini.c2tc.jr_developer;
+
+public class KIA_SpeedLimit {
+
+	  protected void display_speed()
+		{
+			System.out.println("All KIA have speed limit");
+		}
+		  public static void main(String[] args) {
+			  
+			  KIA_AirBags ob2=new KIA_AirBags();
+			  ob2.display_Air();
+}
+
+}
