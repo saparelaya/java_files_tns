d3f0ecf03cd842beb9a28f2febce3b4ef9ef21f9
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-11-30 00:19:09 +0530

    SEVENTH COMMIT

---
+++ Bestiu_Files/src/org/tns/capgemini/c2tc/finaldemo/FinalClassDemo.java
@@ -0,0 +1,22 @@
+package org.tns.capgemini.c2tc.finaldemo;
+
+final class ImplementByLaya
+{
+	public void show() {
+		System.out.println("AI integration by laya");
+	}
+}
+class swetha extends ImplementByLaya 
+{
+	public void show() {
+		System.out.println("AI Inegration done by Swetha");
+	}
+}
+
+public class FinalClassDemo {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Bestiu_Files/src/org/tns/capgemini/c2tc/finaldemo/FinalMethodDemo.java
@@ -0,0 +1,33 @@
+package org.tns.capgemini.c2tc.finaldemo;
+
+class calc
+{
+	public final void show()
+	{
+		System.out.println("AI integration done by laya");
+	}
+	public void add(int a ,int b)
+	{
+		System.out.println(a+b);
+	}
+}
+class Advcalc extends calc
+{
+	public void show()
+	{
+		
+	}
+	public void add(int a ,int b)
+	{
+		System.out.println("welcome");
+	
+	}
+}
+public class FinalMethodDemo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
